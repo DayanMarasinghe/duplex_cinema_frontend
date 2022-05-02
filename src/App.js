3e651b1badb3f6527f3b90deec8bcc34be11a299
Dayan Marasinghe <dayanmarasinghe@gmai.com>
2022-05-02 10:32:56 +0530

    register form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Movieview from './components/Movieview'
 import Register from './components/Register';
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
       <Movieview></Movieview>
+      <Login/>
       <Register/>
     </div>
   );
