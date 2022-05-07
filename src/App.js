64c0e1787c994d9ad94fdb2e69d1655182453312
Dayan Marasinghe <dayanmarasinghe@gmai.com>
2022-05-07 11:26:43 +0530

    login screen cus

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
           <Routes>
             <Route path="/home" element={<Home />}></Route>
             <Route path="/contactus" element={<Contactus />}></Route>
-            <Route path="/userlogin" element={<Userlogin />}></Route>
+            <Route path="/userlogin" element={<Login />}></Route>
             <Route path="/usersignup" element={<Register />}></Route>
           </Routes>
         </Router>
